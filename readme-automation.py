304c417fe7efe67597661608f388d35f0e75b897
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2023-07-18 03:51:19 +0600

    Problem link for .c, .cpp, .js, .java updated

--- readme-automation.py
+++ readme-automation.py
@@ -1,28 +1,21 @@
 import os
 import shutil
 import re
-import subprocess
-from datetime import datetime
-import git, time
-from git.objects.commit import Commit 
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-repo = git.Repo(dir_path)
 
 # Define the problem difficulty levels
 difficulty_levels = {
     "easy": ["A", "B"],
     "medium": ["C", "D", "E"],
-    "hard": ["F", "G", "H", "I"]
+    "hard": ["F", "G", "H", "I", "J", "K", "L"]
 }
 
 print("[+] Moving files to the appropriate folders...")
+
 # Move files to the appropriate folders and update the README files
 for filename in os.listdir("."):
     if filename.endswith(".py"):
         file_path = os.path.join(".", filename)
-        
+
         if filename.endswith(".py"):
             problem_index = filename[:-3]
         elif filename.endswith(".cpp"):
@@ -42,30 +35,16 @@ for filename in os.listdir("."):
                 if os.path.exists(new_file_path):
                     print(f"[+] Replacing existing file: {new_file_path}")
                     os.remove(new_file_path)
-
                 shutil.move(file_path, new_file_path)
 
-                # Retrieve the problem link from the first line of the file
-                problem_link = ""
-                with open(new_file_path, "r") as problem_file:
-                    first_line = problem_file.readline().strip()
-                    match = re.search(r"#\s*(.*)", first_line)
-                    if match:
-                        problem_link = match.group(1)
-
-                # Update the README file in the corresponding folder
-                readme_path = os.path.join(folder_path, "README.md")
-
-                with open(readme_path, "a") as readme_file:
-                    readme_file.write(f"{problem_index} | {problem_link} | {datetime.today().strftime('%d-%m-%Y')}\n")
-                break
-
 print("[+] Updating subfolder README files...")
+
 # Update the subfolder README files with the indexed file information
 for difficulty in difficulty_levels.keys():
     folder_path = os.path.join(".", difficulty)
     readme_path = os.path.join(folder_path, "README.md")
-    file_list = sorted(file for file in os.listdir(folder_path) if file != "README.md")
+    file_list = sorted(file for file in os.listdir(
+        folder_path) if file != "README.md")
 
     with open(readme_path, "w") as readme_file:
         readme_file.write("Problem | Codeforces Link |\n")
@@ -73,26 +52,21 @@ for difficulty in difficulty_levels.keys():
 
         for filename in file_list:
             problem_index = filename[:-3]
-            problem_link = ""
 
-            with open(os.path.join(folder_path, filename), "r") as problem_file:
-                first_line = problem_file.readline().strip()
-                match = re.search(r"#\s*(.*)", first_line)
-                if match:
-                    problem_link = match.group(1)
+            problem_link = ""
+            try:
+                with open(os.path.join(folder_path, filename), "r") as problem_file:
+                    first_line = problem_file.readline().strip()
+                    match = re.search(r"(#|\/\/)\s*(.*)", first_line)
+                    if match:
+                        problem_link = match.group(2)
+                    else:
+                        problem_link = "Not found!"
+            except:
+                problem_link = "Not found!"
 
             readme_file.write(f"[{problem_index}]({filename}) | {problem_link} |\n")
-    
+
     readme_file.close()
-    
-# Push to GitHub
-# commit_message = "Updated by automated commit 🤖"
-# repo.git.add('--all')
-# repo.git.commit('-m', commit_message, author='Shakib')
-# print('[*] Pushing......')
-# origin = repo.remote(name='origin')
-# origin.push()
-# time.sleep(1)
-# print("[=] Successfull")
 
 print("[=] Readme update successful.")
